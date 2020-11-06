4c7760ed8d169275e4b2907c9bcb025229aa9442
bwapes <benwaples@gmail.com>
2020-11-05 16:05:15 -0800

    findAll and finById routes

--- __tests__/state.test.js
+++ __tests__/state.test.js
@@ -1,6 +1,7 @@
 require('../lib/data/data-helper');
 const request = require('supertest');
 const app = require('../lib/app');
+const State = require('../lib/models/State');
 
 describe('state-trackr-be routes', () => {
 
@@ -18,5 +19,18 @@ describe('state-trackr-be routes', () => {
         dateVisited: '10-10-15',
         wasFun: true
       }));
+  });
+
+  it('should return all states via GET', async() => {
+    return request(app)
+      .get('/api/v1/states')
+      .then(res => expect(res.body.length).toEqual(20));
+  });
+
+  it('should return a single state by id via GET', async() => {
+    const firstState = (await State.find())[0];
+    return request(app)
+      .get(`/api/v1/states/${firstState.id}`)
+      .then(res => expect(res.body).toEqual(firstState));
   });
 });
